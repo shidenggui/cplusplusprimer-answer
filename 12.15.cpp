0901efaafbf026f723f5019887c0c6ebe7c85127
shidenggui <903618848@qq.com>
2014-12-29 22:30:52 +0800

    finish ex12.13 12.14 12.15

---
+++ 12.15.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+struct destination;
+struct connection;
+connection connect(destination *);
+void disconnect(connection);
+
+void f(destination &d){
+    connection c = connect(&d);
+    shared_ptr<connection> p(&c, [](connection *p){disconnect(*p)});
+}
+int main(){
+
+    return 0;
+}
