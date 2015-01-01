32b4c782a6662c0aa07bbe3ad835714483f65b84
shidenggui <903618848@qq.com>
2015-01-01 23:26:26 +0800

    fix ex12.19 error

---
+++ 12.19.strblobptr.cpp
@@ -0,0 +1,20 @@
+#include "12.19.strblobptr.h"
+shared_ptr<vector<string>> StrBlobPtr::check(size_t i, const string &msg) const {
+     auto ret = wptr.lock();
+     if(!ret){
+         throw std::runtime_error("unbound StrBlobPtr");
+     }
+     if(i >= ret->size()){
+          throw std::out_of_range(msg);
+     }
+     return ret;
+}
+string& StrBlobPtr::deref() const {
+     auto p = check(curr, "dereference past end");
+     return (*p)[curr];
+}
+StrBlobPtr& StrBlobPtr::incr(){
+     check(curr, "increment past end of StrBlobPtr");
+     ++curr;
+     return *this;
+}
